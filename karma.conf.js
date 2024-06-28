fb75989743fda94317b22461cfa6f717a224a55c
CatherineGomez-AD <catherine.gomez@training.assuresoft.com>
2024-06-28 10:14:52 -0400

    fix: lint problems

--- karma.conf.js
+++ karma.conf.js
@@ -1,31 +1,23 @@
 module.exports = function(config) {
     config.set({
       basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      frameworks: ['jasmine', 'angular-cli'], // Ajusta los frameworks según sea necesario
       plugins: [
         require('karma-jasmine'),
         require('karma-chrome-launcher'),
         require('karma-jasmine-html-reporter'),
         require('karma-coverage'),
-        require('@angular-devkit/build-angular/plugins/karma'),
+        // No incluyas require('@angular-devkit/build-angular/plugins/karma')
         require('karma-junit-reporter')
       ],
-      client: {
-        clearContext: false // leave Jasmine Spec Runner output visible in browser
-      },
-      preprocessors: {
-        'src/**/*.js': ['coverage'] // Ajusta esto según la ubicación de tu código fuente
-      },
-      coverageReporter: {
-        type: 'cobertura', // Tipo XML para Jenkins
-        dir: 'coverage/',
-        file: 'coverage.xml'
-      },
+      // Ajusta el resto de la configuración según tu proyecto
       reporters: ['progress', 'kjhtml', 'coverage', 'junit'],
       junitReporter: {
-        outputDir: 'test-results', // Directorio donde se generará el archivo
+        outputDir: 'test-results/',
         outputFile: 'test-results.xml',
-        useBrowserName: false
+        useBrowserName: false,
+        suite: '',
+        xmlVersion: '1'
       },
       port: 9876,
       colors: true,
